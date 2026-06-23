dfd775810134c295a4770ba34340f3bfb9345f07
Faizaan <iamrevenent007@gmail.com>
2026-06-24 03:31:18 +0530

    Expand calendar display hours from 6:00 AM to 9:00 PM to show early/late blocks

--- frontend/src/components/Dashboard/CalendarPage.jsx
+++ frontend/src/components/Dashboard/CalendarPage.jsx
@@ -28,9 +28,9 @@ export default function CalendarPage({ tasks }) {
 
   const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
   const hours = [
-    '09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', 
-    '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', 
-    '05:00 PM', '06:00 PM'
+    '06:00 AM', '07:00 AM', '08:00 AM', '09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', 
+    '01:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM', '06:00 PM', '07:00 PM',
+    '08:00 PM', '09:00 PM'
   ];
 
   // Layer filters
